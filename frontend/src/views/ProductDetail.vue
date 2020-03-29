2278a404ad7402374be3c8efe0a03c0ccd0009d1
regi18 <regi18@libero.it>
2020-03-29 19:20:01 +0200

    feat(vue): initial pages & components

---
+++ frontend/src/views/ProductDetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="ProductDetail">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductDetail',
+}
+</script>
---
+++ frontend/src/views/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Products">
+    <v-row justify="center" align="center">
+      <v-col v-for="i in 20" :key="i" cols="3">
+        <v-card>
+          <v-card-title>
+            Prodotto
+          </v-card-title>
+          <v-card-text>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Products"
+};
+</script>
