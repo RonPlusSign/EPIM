7c3531e975c1ae2477e9c8e16e592e4d6e5823bd
RonPlusSign <andrea.delli2001@gmail.com>
2020-03-29 18:36:45 +0200

    Created Products, Category and ProductImage tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,22 @@
+DROP DATABASE epim IF EXISTS;
+CREATE DATABASE epim IF NOT EXISTS;
+USE epim;
+
+CREATE TABLE Category (
+    category_id INT          PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(128)             NOT NULL
+);
+
 CREATE TABLE Product (
-    product_id INT PRIMARY KEY,
-    name VARCHAR(128) NOT NULL,
-    description VARCHAR(128) NOT NULL,
-    price VARCHAR(128) NOT NULL,
-    quantity INT NOT NULL,
-    category INT NOT NULL foreign key
+    product_id        INT PRIMARY KEY   NOT NULL AUTO_INCREMENT,
+    name              VARCHAR(256)      NOT NULL,
+    description       VARCHAR(4096)     NOT NULL,
+    price             FLOAT             NOT NULL,
+    quantityAvailable INT               NOT NULL,
+    category          INT               NOT NULL FOREIGN KEY REFERENCES Category(category_id) );
+
+CREATE TABLE ProductImage(
+    imageFile VARBINARY(MAX) NOT NULL, -- Usare MAX per contenere immagini di più di 8000 byte
+                                       -- TODO: Valutare di salvare soltanto il path dell'immagine
+    product_id INT           NOT NULL FOREIGN KEY REFERENCES Product(product_id)
 );
