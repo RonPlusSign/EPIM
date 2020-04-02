a5f204c538dd959f62a5047e91ee311fbd93cb9a
regi18 <regi18@libero.it>
2020-04-02 21:50:11 +0200

    feat(php): database class & bootstrap
    also created a rude folder structure

---
+++ backend/Bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+// Comment this line in production
+//$DEBUG_MODE = true;
+
+if (isset($DEBUG_MODE)) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
+else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
+
+// Session security configurations
+ini_set('session.use_only_cookies', TRUE);
+ini_set('session.use_trans_sid', FALSE);
+ini_set('session.cookie_lifetime', 0); // In seconds. 0 = Session timeout (this because we manage session timeout ourselves)
+
+// POST settings
+ini_set('post_max_size', '10M');
+ini_set('upload_max_filesize', '10M');
+ini_set('max_file_uploads', 1);
+
+require_once __DIR__ . '/lib/Database.php';
---
+++ backend/products/Products.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Author: regi18
+
+    Get the list of products bases on a filter:
+
+        - by title          ?q=
+        - by category       ?c=
+        - by brand          ?b=
+        - by price range    ?ps=x&pe=x  (price start and price end)
+
+ */
+
+require_once __DIR__ . '/../Bootstrap.php';
