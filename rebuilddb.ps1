f520205a579b45598336e711e7b4f9b453fd0a0d
regi18 <regi18@libero.it>
2020-04-03 22:08:07 +0200

    build: added istat-data to rebuilddb script

--- rebuilddb.ps1
+++ rebuilddb.ps1
@@ -1,4 +1,7 @@
 echo "[*] Uploading schema"
 echo "DROP DATABASE epim;" | docker exec -i epim-db /usr/bin/mysql -u epim --password=epim epim
 cat ./database/schema.sql | docker exec -i epim-db /usr/bin/mysql -u epim --password=epim
+echo "[+] Done"
+echo "[*] Inserting data"
+cat ./database/istat_data.sql | docker exec -i epim-db /usr/bin/mysql -u epim --password=epim
 echo "[+] Done" 
