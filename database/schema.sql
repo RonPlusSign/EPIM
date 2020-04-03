323944e9c6534891aac0831c2ee49cec13122d5a
RonPlusSign <andrea.delli2001@gmail.com>
2020-04-03 17:20:49 +0200

    impr(sql) & refactor(sql): created orders tables and refactored old ones

--- database/schema.sql
+++ database/schema.sql
@@ -1,125 +1,213 @@
 CREATE DATABASE IF NOT EXISTS epim;
 USE epim;
 
--- Products
+-- Name conventions:
+-- Table names and attributes: lowercase and snake_case
 
-CREATE TABLE IF NOT EXISTS product (
-    id INT(10) UNSIGNED PRIMARY KEY NOT NULL,
-    title VARCHAR(128) NOT NULL,
-    description VARCHAR(512) NOT NULL,
-    purchase_price FLOAT NOT NULL,
-    sell_price FLOAT NOT NULL,
-    recommended_price FLOAT NOT NULL,
-    quantity INT(10) UNSIGNED NOT NULL,
-    category            INT(10) UNSIGNED               NOT NULL,
-    CONSTRAINT `fk_category`
+-- Always define foreign and primary keys as CONSTRAINT
+-- Costraints name conventions:
+-- CONSTRAINT `thistable_fk_referencedtable` FOREIGN KEY(column) REFERENCES [...]
+-- CONSTRAINT `thistable_pk` PRIMARY KEY(columns)
+-- CONSTRAINT `thistable_unique` UNIQUE(columns)    // you can also use `columnname_unique`
+
+
+------------------------- Products -------------------------
+
+CREATE TABLE IF NOT EXISTS category
+(
+    id   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128)     NOT NULL,
+
+    CONSTRAINT `category_pk` PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS brand
+(
+    id   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128)     NOT NULL,
+
+    CONSTRAINT `brand_pk` PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS product
+(
+    id                INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    title             VARCHAR(128)     NOT NULL,
+    description       VARCHAR(512)     NOT NULL,
+    purchase_price    FLOAT            NOT NULL,
+    sell_price        FLOAT            NOT NULL,
+    recommended_price FLOAT            NOT NULL,
+    quantity          INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    category          INT(10) UNSIGNED NOT NULL,
+    brand             INT(10) UNSIGNED NOT NULL,
+
+    CONSTRAINT `product_pk` PRIMARY KEY (id),
+    CONSTRAINT `product_fk_category`
         FOREIGN KEY (category) REFERENCES category (id)
             ON DELETE RESTRICT
             ON UPDATE RESTRICT,
-    brand            INT(10) UNSIGNED               NOT NULL,
-    CONSTRAINT `fk_brand`
+    CONSTRAINT `product_fk_brand`
         FOREIGN KEY (brand) REFERENCES brand (id)
             ON DELETE RESTRICT
-            ON UPDATE RESTRICT,
+            ON UPDATE RESTRICT
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS product_image (
-    product INT(10) UNSIGNED PRIMARY KEY NOT NULL,
-    url VARCHAR(128) NOT NULL,
-    CONSTRAINT `fk_product`
+CREATE TABLE IF NOT EXISTS product_image
+(
+    product INT(10) UNSIGNED NOT NULL,
+    url     VARCHAR(128)     NOT NULL,
+
+    CONSTRAINT `product_image_pk` PRIMARY KEY (product, url),
+    CONSTRAINT `product_image_fk_product`
         FOREIGN KEY (product) REFERENCES product (id)
             ON DELETE CASCADE
-            ON UPDATE CASCADE,
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS category (
-    id INT(10) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(128)                 NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS brand (
-    id INT(10) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(128)                 NOT NULL
+            ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
 
+------------------------- Users -------------------------
 
--- Users
+CREATE TABLE IF NOT EXISTS user
+(
+    id           INT(10) UNSIGNED NOT NULL,
+    email        VARCHAR(128)     NOT NULL,
+    name         VARCHAR(128)     NOT NULL,
+    surname      VARCHAR(128)     NOT NULL,
+    phone_number VARCHAR(128)     NOT NULL,
+    password     VARCHAR(255)     NOT NULL,
+    is_admin     BOOLEAN          NOT NULL,
 
-CREATE TABLE IF NOT EXISTS user (
-    id INT(10) UNSIGNED PRIMARY KEY NOT NULL,
-    email VARCHAR(128) NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    surname VARCHAR(128) NOT NULL,
-    phone_number VARCHAR(128) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN NOT NULL,
+    CONSTRAINT `user_pk` PRIMARY KEY (id),
+    CONSTRAINT `email` UNIQUE (email)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS cart (
-    user INT(10) UNSIGNED NOT NULL,
-    product INT(10) UNSIGNED NOT NULL,
-    quantity INT(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS cart
+(
+    user     INT(10) UNSIGNED NOT NULL,
+    product  INT(10) UNSIGNED NOT NULL,
+    quantity INT(10) UNSIGNED NOT NULL DEFAULT 1,
 
     CONSTRAINT `cart_pk` PRIMARY KEY (user, product),
-    CONSTRAINT `product_fk` 
+    CONSTRAINT `product_fk`
         FOREIGN KEY (product) REFERENCES product (id)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
-    CONSTRAINT `user_fk` 
+    CONSTRAINT `cart_fk_user`
         FOREIGN KEY (user) REFERENCES user (id)
             ON DELETE CASCADE
-            ON UPDATE CASCADE,
-
+            ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
 
+------------------------- Addresses -------------------------
 
--- Addresses
+CREATE TABLE IF NOT EXISTS region
+(
+    id   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32)      NOT NULL,
 
-CREATE TABLE IF NOT EXISTS region (
-    id INT(10) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(128)                 NOT NULL
+    CONSTRAINT `region_pk` PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS province (
-    id CHAR(2) PRIMARY KEY NOT NULL,
-    name        VARCHAR(128)                 NOT NULL
-) ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS province
+(
+    id     CHAR(2)          NOT NULL,
+    name   VARCHAR(64)      NOT NULL,
+    region INT(10) UNSIGNED NOT NULL,
 
-CREATE TABLE IF NOT EXISTS address (
-    id BIGINT NOT NULL,
-    street VARCHAR(255) NOT NULL, 
-    house_number INT(10) NOT NULL, 
-    postal_code INT(10) NOT NULL, 
-    city VARCHAR(255) NOT NULL, 
-    province INT(10) NOT NULL, 
-    region INT(10) NOT NULL,
-
-    CONSTRAINT `address_pk` PRIMARY KEY (id, street, house_number, postal_code, city, province, region),
-    CONSTRAINT `province_fk` 
-        FOREIGN KEY (province) REFERENCES province (id)
-            ON DELETE RESTRICT
-            ON UPDATE CASCADE,
-    CONSTRAINT `region_fk` 
+    CONSTRAINT `province_pk` PRIMARY KEY (id),
+    CONSTRAINT `province_fk_region`
         FOREIGN KEY (region) REFERENCES region (id)
-            ON DELETE RESTRICT
-            ON UPDATE CASCADE,
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS user_address (
-    address BIGINT NOT NULL, 
-    user INT(10) NOT NULL,
-    phone_number VARCHAR(128) NOT NULL,
+CREATE TABLE IF NOT EXISTS city
+(
+    id       INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(64)      NOT NULL,
+    province CHAR(2)          NOT NULL,
+
+    CONSTRAINT `city_pk` PRIMARY KEY (id),
+    CONSTRAINT `city_fk_province`
+        FOREIGN KEY (province) REFERENCES province (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS address
+(
+    id           INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    street       VARCHAR(128)     NOT NULL,
+    house_number INT(10) UNSIGNED NOT NULL,
+    postal_code  INT(10) UNSIGNED NOT NULL,
+    city         INT(10) UNSIGNED NOT NULL,
+
+    CONSTRAINT `address_unique` UNIQUE (street, house_number, postal_code, city),
+    CONSTRAINT `address_pk` PRIMARY KEY (id),
+    CONSTRAINT `address_fk_city`
+        FOREIGN KEY (city) REFERENCES city (id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS user_address
+(
+    address      INT(10) UNSIGNED NOT NULL,
+    user         INT(10) UNSIGNED NOT NULL,
+    phone_number VARCHAR(32)      NOT NULL,
 
     CONSTRAINT `user_address_pk` PRIMARY KEY (address, user),
-    CONSTRAINT `address_fk` 
+    CONSTRAINT `user_address_fk_address`
         FOREIGN KEY (address) REFERENCES address (id)
             ON DELETE RESTRICT
             ON UPDATE CASCADE,
-    CONSTRAINT `user_fk` 
+    CONSTRAINT `user_address_fk_user`
+        FOREIGN KEY (user) REFERENCES user (id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+
+------------------------- Orders -------------------------
+
+CREATE TABLE IF NOT EXISTS order
+(
+    id            INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    user          INT(10) UNSIGNED NOT NULL,
+    address       INT(10) UNSIGNED NOT NULL,
+    phone_number  VARCHAR(64)      NOT NULL, -- Stored because it could be useful during the shipping
+    timestamp     DATETIME         NOT NULL,
+    shipping_cost FLOAT            NOT NULL,
+    status        VARCHAR(64)      NOT NULL,
+
+    CONSTRAINT `order_pk` PRIMARY KEY (id),
+    CONSTRAINT `order_fk_user`
         FOREIGN KEY (user) REFERENCES user (id)
             ON DELETE RESTRICT
             ON UPDATE CASCADE,
-) ENGINE = InnoDB;
+    CONSTRAINT `order_fk_address`
+        FOREIGN KEY (address) REFERENCES address (id)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE
+);
 
+CREATE TABLE IF NOT EXISTS order_detail
+(
+    order         INT(10) UNSIGNED NOT NULL,
+    product       INT(10) UNSIGNED,
+    quantity      INT(10) UNSIGNED NOT NULL,
+    product_title VARCHAR(128)     NOT NULL,
+    product_price FLOAT            NOT NULL,
+    brand_name    VARCHAR(128)     NOT NULL,
+
+    CONSTRAINT `order_detail_pk` PRIMARY KEY (order, product),
+    CONSTRAINT `order_detail_fk_order`
+        FOREIGN KEY (order) REFERENCES order (id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+    CONSTRAINT `order_detail_fk_product`
+        FOREIGN KEY (product) REFERENCES product (id)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
+);
