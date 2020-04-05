f3adcfde18c65f69a6062586a67bfa3998de9fd7
regi18 <regi18@libero.it>
2020-04-05 15:45:17 +0200

    feat(php): price filter, single product, pages

--- backend/classes/ProductsHandler.php
+++ backend/classes/ProductsHandler.php
@@ -1,26 +1,6 @@
 <?php
 /*
     Author: regi18
-
-    Get the list of products bases on a filter:
-
-        - by title          ?q=
-        - by category       ?c=
-        - by brand          ?b=
-        - by price range    ?ps=x&pe=x  (price start and price end)
-
-    Order by:
-        - Order flag                    ?sort
-        - order by price ascendent      ?price-asc
-        - order by price descendent     ?price-desc
-        - order by title ascentent      ?title-asc
-        - order by title descentent     ?title-desc
-
-
-
-    Pagination: ?p=    (Example ?p=1 page one, it will show an arbitrary amount of products per page)
-
-
  */
 
 require_once __DIR__ . '/../lib/Bootstrap.php';
@@ -35,23 +15,71 @@ class ProductsHandler
     private $isOrderAsc;
     private $extraFilter;
     private $extraFilterString;
+    private $isPriceRangeFilter;
+    private $priceRangeFilterMin;
+    private $priceRangeFilterMax;
+    private $pageNumber;
+
 
     public function __construct()
     {
         $this->isSort = false;
         $this->extraFilter = NULL;
+        $this->pageNumber = 0;
     }
 
-
+    /**
+     * Sets how many products will be returned for every page
+     * 
+     * @param int $limit The number of priducts
+     */
     public function setResultPerPageLimit($limit)
     {
         self::$productsPerPage = $limit;
     }
 
+    public function getResultPerPageLimit()
+    {
+        return self::$productsPerPage;
+    }
+
+    /**
+     * Sets the page number
+     * 
+     * @param int $page
+     */
+    public function setPageNumber($page)
+    {
+        $this->pageNumber = $page;
+    }
+
+    /**
+     * Sets an additional filter. Available options:
+     * 'brand', 'category'
+     * 
+     * @param string $filter Filter (choose between brand and category)
+     * @param string $filterString The parameter of the filter
+     */
     public function setExtraFilter($filter, $filterString)
     {
         $this->extraFilter = $filter;
         $this->extraFilterString = $filterString;
+    }
+
+    /**
+     * Activates and deactivates price range filter
+     * To deactivate call this function with one parameter:
+     * setPriceRangeFilter(false);
+     * 
+     * @param bool $isActive status of the filter
+     * @param float $minimum Minimum price
+     * @param float $maximum Maximum price
+     */
+    public function setPriceRangeFilter($isActive, $minimum = 0, $maximum = 0)
+    {
+        $this->isPriceRangeFilter = $isActive;
+        $this->priceRangeFilterMin = $minimum;
+        $this->priceRangeFilterMax = $maximum;
     }
 
 
@@ -74,41 +102,39 @@ class ProductsHandler
 
 
 
-
-    private function queryByFilterString($filter, $filterString)
+    /**
+     * Match products by string
+     * 
+     * @param string $filter The column name on which to search (available: title)
+     * @param string $filterString The string to search for
+     * 
+     * @return Array Filtered data
+     */
+    private function queryByFilterString($filter, $filterString, $countResults = false)
     {
-        // $query = "SELECT * FROM product
-        // INNER JOIN category ON product.category=category.id
-        // INNER JOIN brand ON product.brand=brand.id
-        // WHERE MATCH (" . $filter . ") AGAINST (:filterString IN NATURAL LANGUAGE MODE)";
+        // Query
+        if ($countResults) {
+            $query = "SELECT count(*) as number_of_products FROM product";
+        } else {
+            $query = "SELECT product.*, brand.name as brand, category.name as category FROM product";
+        }
 
-        // $query = "SELECT * FROM (
-        //     SELECT p.id, p.title, p.description, p.purchase_price, p.sell_price, p.recommended_price, p.quantity, c.name as category, b.name as brand FROM product as p
-        //     INNER JOIN category as c ON p.category=c.id
-        //     INNER JOIN brand as b ON p.brand=b.id) AS pf ";
-            
-        // if ($this->extraFilter == 'category') {
-        //     $query .= "WHERE c.name = ':extraFilter'";
-        // }
-        // else if ($this->extraFilter == 'brand') {
-        //     $query .= "WHERE b.name = ':extraFilter'";
-        // }
-        
-        //$query .= "WHERE MATCH (" . $filter . ") AGAINST (:filterString IN NATURAL LANGUAGE MODE)";
+        $query .= " INNER JOIN category ON product.category=category.id
+                    INNER JOIN brand ON product.brand=brand.id
+                    WHERE MATCH (" . $filter . ") AGAINST (:filterString IN NATURAL LANGUAGE MODE)";
 
-        $query = "SELECT product.*, brand.name as brand, category.name as category FROM product
-        INNER JOIN category ON product.category=category.id
-        INNER JOIN brand ON product.brand=brand.id
-        WHERE MATCH (" . $filter . ") AGAINST (:filterString IN NATURAL LANGUAGE MODE)";
 
+        // Check for extra filter
         if ($this->extraFilter == 'category') {
             $query .= " AND category.name = :extraFilter";
-        }
-        else if ($this->extraFilter == 'brand') {
+        } else if ($this->extraFilter == 'brand') {
             $query .= " AND brand.name = :extraFilter";
         }
-        
 
+        // Check for price filter
+        if ($this->isPriceRangeFilter) {
+            $query .= " AND product.sell_price >= :minPrice AND product.sell_price <= :maxPrice";
+        }
 
         // check if sort 'activated' 
         if ($this->isSort) {
@@ -119,32 +145,62 @@ class ProductsHandler
             else $query .= ' DESC';
         }
 
-        $query .= ' LIMIT :productsPerPage';
+        if (!$countResults) $query .= ' LIMIT :limitOffset, :productsPerPage';
 
         $stm = Database::$pdo->prepare($query);
         $stm->bindParam(':filterString', $filterString);
-        $stm->bindParam(':productsPerPage', self::$productsPerPage);
 
+        if (!$countResults) {
+            $stm->bindParam(':productsPerPage', self::$productsPerPage);
+            $limitOffset = $this->pageNumber * self::$productsPerPage;
+            $stm->bindParam(':limitOffset', $limitOffset);
+        }
+
+        // Bind extraFilter params
         if ($this->extraFilter != NULL) $stm->bindParam(':extraFilter', $this->extraFilterString);
-
+        // Bind price filter param
+        if ($this->isPriceRangeFilter != NULL) {
+            $stm->bindParam(':minPrice', $this->priceRangeFilterMin);
+            $stm->bindParam(':maxPrice', $this->priceRangeFilterMax);
+        }
 
         $stm->execute();
         return $stm->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    /**
+     * This function does a similar query to "getByTitle" 
+     * but it returns only the total number of rows (without any limit)
+     */
+    public function getNumberOfProducts($title)
+    {
+        return self::queryByFilterString('title', $title, true)[0]["number_of_products"];
+    }
 
     public function getByTitle($title)
     {
         return self::queryByFilterString('title', $title);
     }
 
-    public function filterByCategory($category)
-    {
-        return self::queryByFilterString('category', $category);
+
+    /**
+     * Get a single product
+     * 
+     * @param int $id The product's id
+     * 
+     * @return Array the product
+     */
+    public function getProduct($id) {
+        $stm = Database::$pdo->prepare("SELECT product.*, brand.name as brand, category.name as category FROM product
+                                        INNER JOIN category ON product.category=category.id
+                                        INNER JOIN brand ON product.brand=brand.id
+                                        WHERE id=:id");
+        $stm->bindParam(':id', $id);
+        $stm->execute();
+        return $stm->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function filterByBrand($brand)
-    {
-        return self::queryByFilterString('category', $category);
+    public function setProductQuantity($id, $quantity) {
+
     }
 }
--- backend/endpoints/products.php
+++ backend/endpoints/products.php
@@ -2,24 +2,7 @@
 /*
     Author: regi18
 
-    Get the list of products bases on a filter:
-
-        - by title          ?q=
-        - by category       ?c=
-        - by brand          ?b=
-        - by price range    ?ps=x&pe=x  (price start and price end)
-
-    Order by:
-        - Order flag                    ?sort
-        - Order ASC or DESC             ?asc or ?desc
-        - order by pric                 ?s-price
-        - order by titl                 ?s-title
-
-
-
-    Pagination: ?p=    (Example ?p=1 page one, it will show an arbitrary amount of products per page)
-
-
+    See below for options
  */
 
 require_once __DIR__ . '/../classes/ProductsHandler.php';
@@ -31,37 +14,84 @@ header("Content-Type: application/json");
 
 
 switch ($requestMethod) {
+        /**
+     *  Get the list of products bases on a filter:
+     *
+     *      - by title          ?q=
+     *      - by category       ?c=
+     *      - by brand          ?b=
+     *      - by price range    ?ps=x&pe=x  (price start and price end)
+     *
+     *  Order by:
+     *      - Order flag                    ?sort
+     *      - Order ASC or DESC             ?asc or ?desc
+     *      - order by pric                 ?s-price
+     *      - order by titl                 ?s-title
+     *
+     *  Pagination: ?p=    (Example ?p=0 page one, it will show an arbitrary amount of products per page)
+     * 
+     *  Single product: ?id=
+     *
+     */
     case 'GET':
 
         // Check for sorting settings
         if (isset($_GET['sort'])) {
             $isAsc = true;
             $orderBy = '';
-            
+
             if (isset($_GET['desc'])) $isAsc = false;
 
             if (isset($_GET['s-price'])) $orderBy = 'price';
             else if (isset($_GET['s-title'])) $orderBy = 'title';
-        
+
             $ph->setSortSettings(true, $isAsc, $orderBy);
         }
 
+        // Category filter
         if (isset($_GET['c'])) {
-            $ph->setExtraFilter('category',$_GET['c']);
+            $ph->setExtraFilter('category', $_GET['c']);
         }
 
+        // Brand filter
         if (isset($_GET['b'])) {
-            $ph->setExtraFilter('brand',$_GET['b']);
+            $ph->setExtraFilter('brand', $_GET['b']);
         }
 
+        // Price range filter
         if (isset($_GET['ps']) && isset($_GET['pe'])) {
+            $ph->setPriceRangeFilter(true, $_GET['ps'], $_GET['pe']);
         }
 
+        if (isset($_GET['p'])) {
+            $ph->setPageNumber($_GET['p']);
+        }
+
+        // Get all products by title filter
         if (isset($_GET['q'])) {
-            echo json_encode($ph->getByTitle($_GET['q']));
+            echo json_encode(array(
+                "totalResults" => $ph->getNumberOfProducts($_GET['q']),
+                "productsPerPage" => $ph->getResultPerPageLimit(),
+                "data" => $ph->getByTitle($_GET['q'])
+            ));
+        }
+        // Get single product
+        else if (isset($_GET['id'])) {
+            echo json_encode(
+                $ph->getProduct($_GET['id'])
+            );
         }
 
         break;
+
+    case 'POST':
+
+        // Set product's quantity
+        if (isset($_GET['set-quantity']) && isset($_GET['id'])) {
+            $json = json_decode(file_get_contents('php://input'));
+            //$ph->setQuantity($_GET['id'], $json->quantity);
+        }
+
 
     default:
         header("HTTP/1.0 405 Method Not Allowed");
