99e0d370de065d4ef925ea89c1d9c39b6e17cf78
RonPlusSign <andrea.delli2001@gmail.com>
2020-04-06 00:25:11 +0200

    wip(vue): basic products view

---
+++ frontend/src/components/EProductListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-card>
+      <v-card-title>{{ this.product.title }}</v-card-title>
+      <v-card-subtitle>{{ this.product.brand }}</v-card-subtitle>
+      <v-card-text>{{ this.product.price }} €</v-card-text>
+    </v-card>
+    <v-divider></v-divider>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "EProductListItem",
+
+  props: ["product"],
+
+  data() {
+    return {
+      product: null
+    };
+  }
+};
+</script>
---
+++ frontend/src/components/EProductsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="!isEmpty">
+    <product-list-item v-for="product in this.products" :product="product" :key="product.id"></product-list-item>
+  </div>
+  <div v-else>
+    <h2 class="text-center">Nessun prodotto trovato!</h2>
+  </div>
+</template>
+
+<script>
+import EProductListItem from "../components/EProductListItem";
+
+export default {
+  name: "EProductsList",
+
+  props: ["products"],
+
+  data() {
+    return {
+      products: []
+    };
+  },
+
+  computed: {
+    isEmpty: function() {
+      return this.products === undefined || this.products.length === 0;
+    }
+  },
+
+  components: {
+    "product-list-item": productListItem
+  }
+};
+</script>
