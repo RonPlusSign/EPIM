45bc0b879ce3adc19ddf72b508bd964afda9b149
RonPlusSign <andrea.delli2001@gmail.com>
2020-04-14 01:18:30 +0200

    wip/feat(vue): added filters to searchbar

--- frontend/src/components/ENavbar.vue
+++ frontend/src/components/ENavbar.vue
@@ -49,13 +49,98 @@
         hide-details
         dense
         append-icon="mdi-magnify"
-        @click:append="searchProduct"
-        @keyup.enter.native="searchProduct"
+        @click:append="searchProducts"
+        @keyup.enter.native="searchProducts"
       ></v-text-field>
 
-      <v-btn icon>
-        <v-icon>mdi-filter</v-icon>
-      </v-btn>
+      <!---------------------------------------->
+      <!---- Filter button & filter inputs ----->
+      <!---------------------------------------->
+      <div class="text-center">
+        <v-menu offset-y :close-on-content-click="false">
+          <!-- filters menu toggler -->
+          <template v-slot:activator="{ on }">
+            <v-btn icon dark v-on="on">
+              <v-icon>mdi-filter</v-icon>
+            </v-btn>
+          </template>
+
+          <!------------------------------->
+          <!----------- filters ----------->
+          <!------------------------------->
+          <v-container class="white">
+            <h3 class="mb-1">Filtri di ricerca</h3>
+            <v-divider></v-divider>
+
+            <!-- Category -->
+            <v-select
+              clearable
+              v-model="selectedCategory"
+              :items="categories"
+              item-text="name"
+              item-value="id"
+              no-data-text="Nessuna categoria presente"
+              prepend-icon="mdi-format-list-bulleted-square"
+              label="Categoria"
+            >
+            </v-select>
+
+            <!-- Brand -->
+            <v-select
+              clearable
+              v-model="selectedBrand"
+              :items="brands"
+              item-text="name"
+              item-value="id"
+              no-data-text="Nessuna marca presente"
+              prepend-icon="mdi-tag"
+              label="Marca"
+            >
+            </v-select>
+
+            <!-- Price range -->
+            <p class="body mt-2 mb-1">Fascia di prezzo</p>
+            <v-range-slider
+              v-model="priceRange.selectedRange"
+              :max="priceRange.max"
+              :min="priceRange.min"
+              :step="priceRange.interval"
+              hide-details
+              thumb-label="always"
+              class="align-center mt-12"
+            >
+              <!-- thumb labels value -->
+              <template v-slot:thumb-label="{ value }">
+                {{ value === priceRange.max ? "MAX" : value }}
+              </template>
+            </v-range-slider>
+
+            <!-- Products order -->
+            <v-select
+              clearable
+              v-model="selectedOrder"
+              :items="typesOfOrder"
+              item-text="name"
+              item-value="type"
+              no-data-text="Nessun ordinamento possibile"
+              prepend-icon="mdi-sort"
+              label="Ordina per..."
+            >
+            </v-select>
+
+            <!-- Filters OK button -->
+            <v-btn
+              class="float-right mt-4 mb-3"
+              dark
+              color="blue"
+              @click="searchProducts"
+            >
+              Cerca
+              <v-icon class="ml-2" dark>mdi-magnify</v-icon>
+            </v-btn>
+          </v-container>
+        </v-menu>
+      </div>
 
       <v-spacer class="hidden-xs-only"></v-spacer>
 
@@ -135,13 +220,7 @@ export default {
   name: "ENavbar",
   data() {
     return {
-      logged: false,
-      user: {
-        // TODO: make a request to login.php to see if the user is logged (and retrieve its data)
-        name: "",
-        id: null,
-      },
-      productSearchQuery: "",
+      // Side menu
       drawerIsExpanded: false,
       drawerItems: [
         { title: "Home", icon: "mdi-home", route: "/" },
@@ -154,28 +233,140 @@ export default {
         { title: "Il tuo profilo", icon: "mdi-account", route: "/profilo" },
         { title: "Logout", icon: "mdi-logout", route: "/logout" },
       ],
+      // User values
+      logged: false,
+      user: {
+        // TODO: make a request to login.php to see if the user is logged (and retrieve its data)
+        name: "",
+        id: null,
+      },
+
+      // ----- Filters attributes -----
+      // Title
+      productSearchQuery: "",
+      // Categories
+      categories: [
+        // { id: 5, name: "Phon" },
+        // { id: 6, name: "Auricolari" },
+        // { id: 7, name: "TV" },
+      ],
+      selectedCategory: null,
+      // Brands
+      brands: [
+        // { id: 1, name: "Samsung" },
+        // { id: 2, name: "Apple" },
+        // { id: 3, name: "Xiaomi" },
+      ],
+      selectedBrand: null,
+      // Price range
+      priceRange: {
+        min: 0,
+        max: 500,
+        interval: 25,
+        selectedRange: [0, 500],
+      },
+      // Order types
+      typesOfOrder: [
+        // TODO: Add order types filter
+      ],
+      selectedOrder: null,
     };
   },
   methods: {
-    searchProduct() {
+    filtersChanged() {
+      let changed =
+        // Title
+        (this.productSearchQuery !== "" &&
+          this.$route.query.q !== this.productSearchQuery.trim()) ||
+        // Category
+        (this.selectedCategory !== null &&
+          this.$route.query.c !== this.selectedCategory) ||
+        // Brand
+        (this.selectedBrand !== null &&
+          this.$route.query.b !== this.selectedBrand);
+
+      // console.log("1: ", changed);
+      if (changed) return changed;
+
+      // Price range
+      // min
+      changed =
+        (this.priceRange.selectedRange[0] !== this.priceRange.min &&
+          this.$route.query.ps === undefined) ||
+        this.priceRange.selectedRange[0] == this.$route.query.ps;
+
+      // console.log("2: ", changed);
+      if (changed) return changed;
+
+      // max
+      changed =
+        (this.priceRange.selectedRange[1] !== this.priceRange.max &&
+          this.$route.query.pe === undefined) ||
+        this.priceRange.selectedRange[1] == this.$route.query.pe;
+
+      // console.log("3: ", changed);
+      return changed;
+      // Order type
+    },
+
+    searchProducts() {
       /**
-       * Block navigation if route is the same (same search) or search is empty string
+       * Block navigation if route is the same (same search)
        * Also redirects to /prodotti when using searchbar
        */
-      if (
-        this.productSearchQuery.trim() !== "" &&
-        this.$route.query.q !== this.productSearchQuery.trim()
-      ) {
+      if (this.filtersChanged()) {
+        var parameters = {};
+
+        if (this.productSearchQuery.trim() !== "")
+          parameters.q = this.productSearchQuery.trim();
+
+        if (this.selectedCategory !== null) {
+          parameters.c = this.selectedCategory;
+        }
+
+        if (this.selectedBrand !== null) {
+          parameters.b = this.selectedBrand;
+        }
+
+        if (this.priceRange.selectedRange[0] !== this.priceRange.min) {
+          parameters.ps = this.priceRange.selectedRange[0];
+        }
+
+        if (this.priceRange.selectedRange[1] !== this.priceRange.max) {
+          parameters.pe = this.priceRange.selectedRange[1];
+        }
+
         this.$router.push({
           name: "products",
-          query: { q: this.productSearchQuery.trim() },
+          query: parameters,
         });
         console.log(this.$router.history.current.fullPath);
       }
     },
   },
   created() {
-    this.productSearchQuery = this.$route.query.q;
+    // TODO: query to get all categories (to fill filters)
+    // TODO: query to get all brands (to fill filters)
+    if (this.$route.query.q !== undefined)
+      this.productSearchQuery = this.$route.query.q;
+    if (this.$route.query.ps !== undefined)
+      this.priceRange.selectedRange[0] = this.$route.query.ps;
+    if (this.$route.query.pe !== undefined)
+      this.priceRange.selectedRange[1] = this.$route.query.pe;
+    // Category
+
+    // TODO: Set the values depending on the current route query string of category and brand (currently not working)
+    if (this.$route.query.c !== undefined)
+      this.selectedCategory = this.$route.query.c;
+    // Brand
+    if (this.$route.query.b !== undefined)
+      this.selectedBrand = this.$route.query.b;
   },
 };
 </script>
+
+<style>
+.v-slider {
+  width: 300px;
+}
+</style>
