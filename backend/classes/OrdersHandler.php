783e532af5d5d63acaeef0bf9ff63b99d9f90022
gate02 <gate8025@gmail.com>
2020-05-18 21:19:13 +0200

    manca solo l'aggiunta
    fatto json getallorders

--- backend/classes/OrdersHandler.php
+++ backend/classes/OrdersHandler.php
@@ -15,13 +15,57 @@ class OrdersHandler
         if(LoginHandler::isAdmin())
         {
             try {
-                $stm = Database::$pdo->prepare("SELECT order_detail.id, order_detail.order_id,order_detail.product_id, order_detail.quantity, order_detail.product_title, order_detail.product_price, order_detail.brand_name, order_history.*, user.email, user.name, user.surname FROM order_detail
+                $stm = Database::$pdo->prepare("SELECT order_detail.order_id,order_detail.product_id, order_detail.quantity, order_detail.product_title, order_detail.product_price, order_detail.brand_name, order_history.*, user.email, user.name, user.surname FROM order_detail
                                                 INNER JOIN order_history ON order_detail.order_id = order_history.id
                                                 INNER JOIN user ON order_history.user = user.id");
                 $stm->execute();
                 $allOrder = $stm->fetchAll(PDO::FETCH_ASSOC);                
                 http_response_code(200);
-                return $allOrder;
+                $array=[];
+                foreach ($allOrder as $orders) {
+                    $trovato = false;
+                    foreach ($array as &$object)  //si fa in questa maniera perchè altrimenti all'esterno del foreach non sarà cambiato nulla
+                    {
+                        if(+$object['id'] === +$orders['order_id'])
+                        {
+                            $trovato = true;
+                            $prodotto = [
+                                "productId" => $orders['product_id'],
+                                "productTitle" => $orders['product_title'],
+                                "quantity" => $orders['quantity'],
+                                "price" => $orders['product_price'],
+                                "brandName" => $orders['brand_name']
+                            ];
+                            $object['products'][] = $prodotto;
+                            
+                        }
+                        
+                    }
+                    if($trovato = false)
+                    {
+                        $datatime = new DateTime($orders['timestamp']);
+                        $user = [
+                            "id" => $orders['user'],
+                            "name" => $orders['name'],
+                            "surname" => $orders['surname'],
+                            "email" => $orders['email']
+                        ];
+                        $array['user'][] = $user;
+                        $nuovoOggetto = [
+                            "id" => $orders['id'],
+                            "date" => $datatime->format('d-m-Y'),
+                            "hour" => $datatime->format('H:i:s'),
+                            "address" => $orders['address'],
+                            "phoneNumber" => $orders['phone_number'],
+                            "shippingCost" => $orders['shipping_cost'],
+                            "status" => $orders['status']
+                        ];
+                        $array[] = $nuovoOggetto;
+                    }
+                }
+                
+                return $array;
+
             } catch (\Exception $e) {
                 echo $e;
             }
